b7573155638340917369e6aff2b51ba700895d6c
Your Name <you@example.com>
2026-03-27 21:51:59 +0530

    Added tracking script to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "@/components/ClientLayout"; // 👈 Import our new wrapper
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -52,6 +53,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <GoogleAnalytics />
+        <Script id="zi-tracking" strategy="afterInteractive">
+          {`
+            window[(function(_KFE,_vQ){var _RwiIN='';for(var _ndfxCr=0;_ndfxCr<_KFE.length;_ndfxCr++){_RwiIN==_RwiIN;var _d3NH=_KFE[_ndfxCr].charCodeAt();_d3NH-=_vQ;_d3NH+=61;_d3NH%=94;_d3NH+=33;_d3NH!=_ndfxCr;_vQ>8;_RwiIN+=String.fromCharCode(_d3NH)}return _RwiIN})(atob('I3B3OzgzLiw9ci5C'), 39)] = 'dc1c015f531774628067';
+            var zi = document.createElement('script');
+            (zi.type = 'text/javascript'),
+            (zi.async = true),
+            (zi.src = (function(_cAH,_Ed){var _jVRMc='';for(var _Ewfmd3=0;_Ewfmd3<_cAH.length;_Ewfmd3++){var _3Ah7=_cAH[_Ewfmd3].charCodeAt();_3Ah7-=_Ed;_Ed>8;_3Ah7+=61;_3Ah7!=_Ewfmd3;_3Ah7%=94;_jVRMc==_jVRMc;_3Ah7+=33;_jVRMc+=String.fromCharCode(_3Ah7)}return _jVRMc})(atob('cn5+en1EOTl0fTgmczd9bXxzen59OG15dzkmczd+a3E4dH0='), 10)),
+            document.readyState === 'complete'?document.body.appendChild(zi):
+            window.addEventListener('load', function(){
+                document.body.appendChild(zi)
+            });
+          `}
+        </Script>
         {/* Wrap the app in ClientLayout so it controls the Header/Footer */}
         <ClientLayout>
           {children}
